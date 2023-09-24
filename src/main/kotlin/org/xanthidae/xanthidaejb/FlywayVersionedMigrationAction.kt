b6228e34019eda567b7b13f59b6af933051cdca5
Frank Schmitt <frankschmitt@gmx.de>
2023-09-24 11:35:31 +0200

    scaffold for versioned migration action

---
+++ src/main/kotlin/org/xanthidae/xanthidaejb/FlywayVersionedMigrationAction.kt
@@ -0,0 +1,22 @@
+package org.xanthidae.xanthidaejb
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileChooser.FileChooser
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
+
+class FlywayVersionedMigrationAction : AnAction() {
+    override fun update(event: AnActionEvent) {
+        // Using the event, evaluate the context,
+        // and enable or disable the action.
+    }
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val descriptor = FileChooserDescriptor(true, false, false, false, false, false)
+        FileChooser.chooseFiles(descriptor, null, null)
+        // Using the event, implement an action.
+        // For example, create and show a dialog.
+    }
+
+    // TODO Override getActionUpdateThread() when you target 2022.3 or later!
+}
